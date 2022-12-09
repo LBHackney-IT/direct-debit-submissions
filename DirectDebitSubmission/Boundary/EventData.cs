d2d880f1065e09f45ea55af08e9daca1a2bb185c
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2022-12-09 15:24:33 +0200

    [HFS-511] - Display Calculations Direct Debit Charge

--- DirectDebitSubmission/Boundary/EventData.cs
+++ DirectDebitSubmission/Boundary/EventData.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace DirectDebitSubmission.Boundary
 {
+    [ExcludeFromCodeCoverage]
     public class EventData
     {
         public object OldData { get; set; }
